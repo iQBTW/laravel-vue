ecff47ed1c8f6b5f8ada29fef630d3eb70404e5e
Rizqy <estrizqy@gmail.com>
2024-04-08 02:37:16 +0700

    menambahkan latihan_studi_kasus_looping php 1, 2, 3

---
+++ PHP/latihan_studi_kasus_looping_3.php
@@ -0,0 +1,13 @@
+<?php
+
+    for ($i = 1; $i <= 10; $i++) {
+        if ($i % 2 == 0) {
+            echo "$i adalah bilangan genap";
+            echo "<br>";
+        } else {
+            echo "$i adalah bilangan ganjil";
+            echo "<br>";
+        }
+    }
+
+?>
