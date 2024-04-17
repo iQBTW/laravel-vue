db049c7c71d455f0258e5b78604d3f9022df3e26
Rizqy <estrizqy@gmail.com>
2024-04-18 03:37:38 +0700

    memperbarui latihan_studi_kasus_array_2

--- PHP/latihan_studi_kasus_array_2.php
+++ PHP/latihan_studi_kasus_array_2.php
@@ -1,6 +1,6 @@
 <?php
 
-    $array = file_get_contents('./json/data.json');
+    $array = file_get_contents('./json/data_studikasus.json');
     $data = json_decode($array, true);
 
     foreach($data as $key => $value){
