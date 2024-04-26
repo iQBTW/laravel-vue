3167a2ac935bab7cb82b4d40c94cbb30bfa0932b
iQ <estrizqy@gmail.com>
2024-04-27 01:11:16 +0700

    progress crud

--- PHP/crud/pengarang.php
+++ PHP/crud/pengarang.php
@@ -1,11 +1,11 @@
 <?php
     include_once("connect.php");
-    $penerbit = mysqli_query($mysqli, "SELECT * FROM penerbit");
+    $pengarang = mysqli_query($mysqli, "SELECT * FROM pengarang");
 ?>
  
 <html>
 <head>    
-    <title>Penerbit</title>
+    <title>Pengarang</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js" integrity="sha384-B0UglyR+jN6CkvvICOB2joaf5I4l3gm9GU6Hc1og6Ls7i6U/mkkaduKaBhlAXv9k" crossorigin="anonymous"></script>
 </head>
@@ -20,26 +20,26 @@
     <hr>
 </center>
 
-<a href="add_penerbit.php">Add New Penerbit</a><br/><br/>
+<a href="add_pengarang.php">Add New Pengarang</a><br/><br/>
  
     <table class="table" width='80%' border=1>
         <tr>
-            <th>ID Penerbit</th> 
-            <th>Nama Penerbit</th> 
+            <th>ID Pengarang</th> 
+            <th>Nama Pengarang</th> 
             <th>Email</th> 
             <th>Telp</th>
             <th>Alamat</th>
             <th>Aksi</th>
         </tr>
     <?php  
-        while($penerbit_data = mysqli_fetch_array($penerbit)) {         
+        while($pengarang_data = mysqli_fetch_array($pengarang)) {         
             echo "<tr>";
-            echo "<td>".$penerbit_data['id_penerbit']."</td>";
-            echo "<td>".$penerbit_data['nama_penerbit']."</td>";
-            echo "<td>".$penerbit_data['email']."</td>";    
-            echo "<td>".$penerbit_data['telp']."</td>";    
-            echo "<td>".$penerbit_data['alamat']."</td>";       
-            echo "<td><a class='btn btn-primary' href='edit_penerbit.php?id_penerbit=$penerbit_data[id_penerbit]'>Edit</a> | <a class='btn btn-danger' href='delete_penerbit.php?id_penerbit=$penerbit_data[id_penerbit]'>Delete</a></td></tr>";        
+            echo "<td>".$pengarang_data['id_pengarang']."</td>";
+            echo "<td>".$pengarang_data['nama_pengarang']."</td>";
+            echo "<td>".$pengarang_data['email']."</td>";    
+            echo "<td>".$pengarang_data['telp']."</td>";    
+            echo "<td>".$pengarang_data['alamat']."</td>";       
+            echo "<td><a class='btn btn-primary' href='edit_pengarang.php?id_pengarang=$pengarang_data[id_pengarang]'>Edit</a> | <a class='btn btn-danger' href='delete_pengarang.php?id_pengarang=$pengarang_data[id_pengarang]'>Delete</a></td></tr>";        
         }
     ?>
     </table>
